ec9e443e4bfcbda99cbe32667989bb11e0b43946
hsohsh <hsohsh@gmail.com>
2025-04-28 23:42:56 +0900

    setting.py 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_n&@7^ysv*^ex86iu#(yx%*_sxuyt1w5p&4f)23kduqd7!95c1"
+SECRET_KEY = config('SECRET_KEY')
+
 
 
 
