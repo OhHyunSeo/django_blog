67174bc7983fb893f251e44fa17df105eb2eb9e1
hsohsh <hsohsh@gmail.com>
2025-04-29 14:35:40 +0900

    최종커밋

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
+
 
 
 def post_share(request, post_id):
@@ -89,3 +92,24 @@ def post_comment(request, post_id):
         comment.save()
     return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
 
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    
+    
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published_objects.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request, 
+                  'blog/post/search.html', 
+                  {'form': form, 
+                   'query': query, 
+                   'results': results})
